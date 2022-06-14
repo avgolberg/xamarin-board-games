d9fd42ad14ffc835e7e45d62a48a74ad25797c8b
avgolberg <av.golberg@gmail.com>
2022-06-14 22:35:59 +0300

    Load Data

---
+++ BGApp/BoardGameDetailPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BGApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BoardGameDetailPage : ContentPage
+    {
+        public BoardGame boardGame;
+        public BoardGameDetailPage(BoardGame boardGame)
+        {
+            this.boardGame = boardGame ?? throw new ArgumentNullException();
+
+            BindingContext = boardGame;
+
+            InitializeComponent();
+           
+        }
+    }
+}
--- BGApp/MainPage.xaml.cs
+++ BGApp/MainPage.xaml.cs
@@ -8,6 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Extended;
 
 namespace BGApp
 {
@@ -21,20 +22,20 @@ namespace BGApp
         public string id { get; set; }
         public string name { get; set; }
         public string[] names { get; set; }
-        public int year_published { get; set; }
-        public int min_players { get; set; }
-        public int max_players { get; set; }
-        public int min_playtime { get; set; }
-        public int max_playtime { get; set; }
-        public int min_age { get; set; }
+        public int? year_published { get; set; }
+        public int? min_players { get; set; }
+        public int? max_players { get; set; }
+        public int? min_playtime { get; set; }
+        public int? max_playtime { get; set; }
+        public int? min_age { get; set; }
         public string description { get; set; }
         public string description_preview { get; set; }
         public string thumb_url { get; set; }
         public string image_url { get; set; }
         public string url { get; set; }
-        public double price { get; set; }
-        public double msrp { get; set; }
-        public double discount { get; set; }
+        public double? price { get; set; }
+        public double? msrp { get; set; }
+        public double? discount { get; set; }
         public PrimaryPublisher primary_publisher { get; set; }
         public IList<Publishers> publishers { get; set; }
         public Mechanic[] mechanics { get; set; }
@@ -42,9 +43,9 @@ namespace BGApp
         public IList<Designers> designers { get; set; }
         public IList<string> developers { get; set; }
         public IList<string> artists { get; set; }
-        public int reddit_all_time_count { get; set; }
-        public int reddit_week_count { get; set; }
-        public int reddit_day_count { get; set; }
+        public int? reddit_all_time_count { get; set; }
+        public int? reddit_week_count { get; set; }
+        public int? reddit_day_count { get; set; }
     }
 
     public class Category
@@ -113,26 +114,44 @@ namespace BGApp
 
     public partial class MainPage : ContentPage
     {
-        const string Url = "https://api.boardgameatlas.com/api/search?client_id=5cTX7InZUl";
+        private int gamesLoaded = 30;
         private HttpClient _client = new HttpClient();
         private ObservableCollection<BoardGame> _boardGames;
         public MainPage()
         {
             InitializeComponent();
             //BGTitle.Text = Resource.BGTitle;
+            LoadData();
         }
 
-        protected override async void OnAppearing()
+        private async void LoadData()
         {
-            base.OnAppearing();
-
+            string Url = "https://api.boardgameatlas.com/api/search?client_id=5cTX7InZUl&limit=30";
             var content = await _client.GetStringAsync(Url);
             var boardGames = JsonConvert.DeserializeObject<Games>(content);
 
-            _boardGames = new ObservableCollection<BoardGame>(boardGames.games);
-
+            _boardGames = new InfiniteScrollCollection<BoardGame>(boardGames.games)
+            {
+                OnLoadMore = async () =>
+                {
+                    gamesLoaded += 30;
+                    Url = "https://api.boardgameatlas.com/api/search?client_id=5cTX7InZUl&limit=30&skip=" + gamesLoaded;
+                    content = await _client.GetStringAsync(Url);
+                    boardGames = JsonConvert.DeserializeObject<Games>(content);
+                    return boardGames.games;
+                }
+            };
             boardGamesListView.ItemsSource = _boardGames;
+        }
 
+        private async void boardGamesListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+                return;
+
+            var boardGame = e.SelectedItem as BoardGame;
+            await Navigation.PushAsync(new BoardGameDetailPage(boardGame));
+            boardGamesListView.SelectedItem = null;
         }
     }
 }
